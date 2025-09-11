1c3a419f23f945f719c585433f3228a3e9dbe982
Isaias <isaiasquintero92141@gmail.com>
2025-09-10 19:07:13 -0500

    Mlops_Inicial

---
+++ main.py
@@ -0,0 +1,46 @@
+from src.preprocessing.functions import expresiones_regulares, tokenizar, lematizar, label_encodering_grupo
+import pandas as pd
+from sqlalchemy import create_engine, text
+from config.settings import DB_CONNECTION_STRING
+import time
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+import string
+
+# Leer el archivo SQL
+with open("sql_queries/queries.sql", "r", encoding="utf-8") as file:
+    query = file.read()
+
+params = {
+    "medico": "PSICOLOGÍA",
+    "fechaini": "2023-01-01",
+    "fechafin": "2025-05-04"
+}
+
+# Ejecutar consulta
+engine = create_engine(DB_CONNECTION_STRING)
+with engine.connect() as conn:
+    historias_clinicas = pd.read_sql(text(query), conn, params=params)
+
+# n para pruebas
+n = 3000
+
+# Procesamiento de texto
+start_time = time.time()
+processed_data = expresiones_regulares(historias_clinicas.head(n), "Concatenada")
+datos_tokenizados = tokenizar(processed_data, "Concatenada")
+datos_lematizados = lematizar(datos_tokenizados, "Concatenada")
+
+# Codificación del grupo
+categotyGrupo, mapping_grupo = label_encodering_grupo(datos_lematizados, "GRUPO", "Label_Grupo")
+
+# Crear columna con texto lematizado como string
+categotyGrupo["Concatenado1"] = categotyGrupo["Concatenada"].apply(lambda x: " ".join(x))
+
+categotyGrupo.to_parquet("datos_modelo.parquet", index=False)
+
+
+
+
---
+++ src/preprocessing/functions.py
@@ -0,0 +1,82 @@
+import re 
+import pandas as pd
+import spacy
+from spacy.lang.es.stop_words import STOP_WORDS as stopwords
+from sklearn import preprocessing
+
+
+
+def expresiones_regulares(df: pd.DataFrame, columna: str) -> pd.DataFrame:
+    df = df.copy()
+    df[columna] = (
+        df[columna]
+        .astype(str)
+        .str.lower()
+        .apply(lambda x: re.sub(r'\s+', ' ', re.sub(r'[^a-zñü ]', '', x)).strip())
+    )
+    return df
+
+
+
+def tokenizar(df: pd.DataFrame, columna: str) -> pd.DataFrame:
+    df = df.copy()
+    nlp = spacy.load("es_core_news_lg")
+
+    stopwords = nlp.Defaults.stop_words
+    stopwords_personalizados = [
+        "medico", "paciente", "psicologo", "psicologa",
+        "psicologia", "psicoterapeuta", "psicoterapia", "refiere"
+    ]
+    stopwords.update(stopwords_personalizados)
+
+    df[columna] = (
+        df[columna]
+        .astype(str)
+        .fillna("")
+        .apply(lambda x: [
+            token.text for token in nlp(x)
+            if token.text.lower() not in stopwords and not token.is_punct and not token.is_space
+        ])
+    )
+    return df
+
+
+
+def lematizar(df: pd.DataFrame, columna: str) -> pd.DataFrame:
+    df = df.copy()
+    nlp = spacy.load("es_core_news_lg")
+
+    df[columna] = df[columna].apply(
+        lambda x: [token.lemma_ for token in nlp(" ".join(x))] if isinstance(x, list) else []
+    )
+    return df
+
+
+
+def label_encodering_sexo(df, columna: str, nueva_columna: str):
+    # crear el codificador
+    label_encoder = preprocessing.LabelEncoder()
+    
+    # Ajustar y transformar la columna
+    df[nueva_columna] = label_encoder.fit_transform(df[columna])
+    
+    # crear el dataframe con el mapeo
+    mapping_sexo = pd.DataFrame({
+        'Sexo': label_encoder.classes_,
+        'Codigo': label_encoder.transform(label_encoder.classes_)
+    })
+    return df, mapping_sexo
+
+def label_encodering_grupo(df, columna: str, nueva_columna: str):
+    # crear el codificador
+    label_encoder = preprocessing.LabelEncoder()
+    
+    # Ajustar y transformar la columna
+    df[nueva_columna] = label_encoder.fit_transform(df[columna])
+    
+    # crear el dataframe con el mapeo
+    mapping_grupo = pd.DataFrame({
+        'Grupo': label_encoder.classes_,
+        'Codigo': label_encoder.transform(label_encoder.classes_)
+    })
+    return df, mapping_grupo
